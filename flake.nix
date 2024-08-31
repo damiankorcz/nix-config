ddc4eb11ce1820e478a402ce632fffb1c048054a
Damian Korcz <damiankorcz28@gmail.com>
2024-08-31 18:32:55 +0100

    Added comments to flake.nix and enabled sops-nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,35 @@
 	description = "Damian's Flake";
 
 	inputs = {
+		# Official NixOS Package Source
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak?ref=<tag> to pin releases.
-		home-manager.url = "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+		# Declarative Flatpak Manager
+		# Use github:gmodena/nix-flatpak?ref=<tag> to pin releases.
+		# https://github.com/gmodena/nix-flatpak
+		nix-flatpak.url = "github:gmodena/nix-flatpak"; # Unstable branch.
+		
+		# System for managing a user environment using the Nix
+		# https://github.com/nix-community/home-manager
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		# Chaotic's Nyx - collection of bleeding-edge and unreleased packages
+		# e.g. linux_cachyos kernel, mesa_git, etc.
+		# https://www.nyx.chaotic.cx/
 		chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
+		# Secrets Management
+		# https://github.com/mic92/sops-nix
+		sops-nix = {
+			url = "github:mic92/sops-nix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = inputs@{ self, nixpkgs, nix-flatpak, home-manager, chaotic }:
+	outputs = inputs@{ self, nixpkgs, nix-flatpak, home-manager, chaotic, sops-nix }:
 	let
 		userSettings = {
 			username = "damian"; # Username
@@ -22,7 +43,7 @@
 		};
 	in {
 		nixosConfigurations = {
-			# Desktop
+			# Desktop (Custom Build)
 			nixos-desktop = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				modules = [
@@ -36,6 +57,8 @@
 						#home-manager.useUserPackages = true;
 					}
 
+					sosp-nix.nixosModules.sops
+
 					chaotic.nixosModules.default
 				];
 
@@ -45,7 +68,7 @@
 				};
 			};
 
-			# Laptop
+			# Laptop (Envy 13 2020)
 			nixos-laptop = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				modules = [
@@ -57,6 +80,8 @@
 					{
 						home-manager.useGlobalPkgs = true;
 					}
+
+					sosp-nix.nixosModules.sops
 				];
 
 				specialArgs = {
@@ -65,7 +90,7 @@
 				};
 			};
 
-			# Raspberry Pi 4
+			# Raspberry Pi 4 (4GB)
 			nixos-pi4 = nixpkgs.lib.nixosSystem {
 				system = "aarch64-linux";
 				modules = [
@@ -77,6 +102,8 @@
 					{
 						home-manager.useGlobalPkgs = true;
 					}
+
+					sosp-nix.nixosModules.sops
 				];
 
 				specialArgs = {
