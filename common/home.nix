cc1e2e58eca3de8b198ffcc62e6a8e09c05db15c
Damian Korcz <damiankorcz28@gmail.com>
2024-09-09 20:30:12 +0100

    Added `video` group to user

--- common/home.nix
+++ common/home.nix
@@ -7,7 +7,7 @@
         isNormalUser = true;
         description = userSettings.name;
         initialPassword = "changeme";
-        extraGroups = [ "networkmanager" "wheel" "adbusers" "kvm" ];
+        extraGroups = [ "networkmanager" "wheel" "adbusers" "kvm" "video" ];
     };
 
     home-manager.users.${userSettings.username} =  { pkgs, ... }: {
