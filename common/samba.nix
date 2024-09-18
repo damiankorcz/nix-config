330d1348048abacfa19070db0e6227d33707949a
Damian Korcz <damiankorcz28@gmail.com>
2024-09-18 19:46:16 +0100

    Samba mount can now be managed by user

--- common/samba.nix
+++ common/samba.nix
@@ -7,15 +7,15 @@
     fileSystems."/mnt/pi4" = {
         device = "//192.168.50.16/Pi4";
         fsType = "cifs";
-        options = [ "credentials=/home/${userSettings.username}/.config/samba/smb-secrets" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" ];
-    };
-
-    # https://discourse.nixos.org/t/cant-mount-samba-share-as-a-user/49171/3
-    security.wrappers."mount.cifs" = {
-      program = "mount.cifs";
-      source = "${lib.getBin pkgs.cifs-utils}/bin/mount.cifs";
-      owner = "root";
-      group = "root";
-      setuid = true;
+        options = [
+            "credentials=/home/${userSettings.username}/.config/samba/smb-secrets"
+            "x-systemd.automount"
+            "noauto"
+            "x-systemd.idle-timeout=60"
+            "x-systemd.device-timeout=5s"
+            "x-systemd.mount-timeout=5s"
+            "uid=1000"
+            "gid=1000"
+        ];
     };
 }
