7f83c4ca26082cfc7dbc631345d94e0ee2ce4e75
Damian Korcz <damiankorcz28@gmail.com>
2024-10-13 20:18:41 +0100

    Updated GRUB.nix

--- common/system/GRUB.nix
+++ common/system/GRUB.nix
@@ -2,14 +2,19 @@
 
 {
     # Use GRUB Bootloader (Needed for dualbooting with Windows across several drives)
-    boot = {
-        loader.systemd-boot.enable = false;
-        loader.grub.enable = true;
-        loader.grub.device = "nodev";
-        loader.grub.useOSProber = true;
-        loader.grub.efiSupport = true;
-        loader.grub.configurationLimit = 8;
-        loader.efi.canTouchEfiVariables = true;
-        loader.efi.efiSysMountPoint = "/boot";
+    boot.loader = {
+        systemd-boot.enable = false;
+        
+        grub = {
+            enable = true;
+            device = "nodev";
+            useOSProber = true;
+            efiSupport = true;
+            configurationLimit = 10;
+            timeoutStyle = hidden; # Hold SHIFT during boot to show GRUB
+        };
+
+        efi.canTouchEfiVariables = true;
+        efi.efiSysMountPoint = "/boot";
     };
 }
