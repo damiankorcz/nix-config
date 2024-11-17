989447fa9795007405fe94cc203ed51d47eea8e5
Damian Korcz <damiankorcz28@gmail.com>
2024-11-17 19:50:06 +0000

    Fixed build of xppen.nix

--- common/software/xppen.nix
+++ common/software/xppen.nix
@@ -1,13 +1,13 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
-    # your other configurations
+    # Temp fix before this is merged: https://github.com/NixOS/nixpkgs/pull/351647
     imports = [
         (import "${inputs.xppen-pr}/nixos/modules/programs/xppen.nix")
     ];
 
     programs.xppen = {
         enable = true;
-        package = (import inputs.xppen-pr { }).xppen_3;
+        package = (import inputs.xppen-pr { inherit (pkgs) system; config.allowUnfree = true; }).xppen_3;
     };
 }
