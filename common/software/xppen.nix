5ed76b7762db8106cb1b3363033858d72095f629
Damian Korcz <damiankorcz28@gmail.com>
2024-11-17 01:23:44 +0000

    Update xppen.nix

--- common/software/xppen.nix
+++ common/software/xppen.nix
@@ -1,13 +1,13 @@
 { pkgs, inputs, ... }:
 
 {
-  # your other configurations
-  imports = [
-    (import "${inputs.xppen-pr}/nixos/modules/programs/xppen.nix")
-  ];
+    # your other configurations
+    imports = [
+        (import "${inputs.xppen-pr}/nixos/modules/programs/xppen.nix")
+    ];
 
-  programs.xppen = {
-    enable = true;
-    package = (import inputs.xppen-pr { }).xppen_3;
-  };
+    programs.xppen = {
+        enable = true;
+        package = (import inputs.xppen-pr { }).xppen_3;
+    };
 }
