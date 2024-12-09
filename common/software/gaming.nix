8c08822c9d0fe129ce29738df72f539ab09d699b
Damian Korcz <damiankorcz28@gmail.com>
2024-12-09 07:53:05 +0000

    Added Sched-ext userspace schedulers

--- common/software/gaming.nix
+++ common/software/gaming.nix
@@ -8,6 +8,12 @@
         protonplus
         libnotify     # Library for Sending Desktop Notifications to the Notification Daemon
         prismlauncher # Launcher for Minecraft
+
+        # Sched-ext userspace Schedulers (Available in Kernel 6.12 or newer)
+        # https://github.com/sched-ext/scx
+        # https://www.kernel.org/doc/html/next/scheduler/sched-ext.html
+        # https://wiki.cachyos.org/configuration/sched-ext/
+        scx.full
     ];
 
 
@@ -29,7 +35,6 @@
         # Optimise system performance on demand
         gamemode.enable = true;
     };
-
 
     # Improve compatibility with memory hungry applications (e.g. games)
     boot.kernel.sysctl."vm.max_map_count" = 1048576;
