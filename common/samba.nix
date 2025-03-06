cb36dc76afce78f3deb2dc4761bd50023fce0f64
Damian Korcz <damiankorcz28@gmail.com>
2025-03-06 00:29:46 +0000

    Reverted the change to defer loading Samba share on boot

--- common/samba.nix
+++ common/samba.nix
@@ -1,23 +1,21 @@
 { pkgs, userSettings, ... }:
 
 {
-    # For mount.cifs, required unless domain name resolution is not needed.
-    environment.systemPackages = [ pkgs.cifs-utils ];
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
 
-    fileSystems."/mnt/pi4" = {
-        device = "//192.168.50.16/Pi4";
-        fsType = "cifs";
-        options = [
-            "credentials=/home/${userSettings.username}/.config/samba/smb-secrets"
-            "x-systemd.automount"
-            "noauto"
-            "x-systemd.idle-timeout=60"
-            "x-systemd.device-timeout=5s"
-            "x-systemd.mount-timeout=5s"
-            "uid=1000"
-            "gid=1000"
-            "x-systemd.after=graphical-session.target"
-            "x-systemd.requires=graphical-session.target"
-        ];
-    };
+  fileSystems."/mnt/pi4" = {
+    device = "//192.168.50.16/Pi4";
+    fsType = "cifs";
+    options = [
+      "credentials=/home/${userSettings.username}/.config/samba/smb-secrets"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "uid=1000"
+      "gid=1000"
+    ];
+  };
 }
