dd75ca07009d3b8cabd92809edbf58c8557047a0
Damian Korcz <damiankorcz28@gmail.com>
2025-03-06 00:37:47 +0000

    Reworked home.nix to remove Home Manager + Added Credentials (libSecret) for Git

--- common/home.nix
+++ common/home.nix
@@ -1,59 +1,55 @@
-{ userSettings, ... }:
+{ userSettings, pkgs, ... }:
 
 {
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.${userSettings.username} = {
-        uid = 1000;
-        isNormalUser = true;
-        description = userSettings.name;
-        initialPassword = "changeme";
-        extraGroups = [ "networkmanager" "wheel" "adbusers" "kvm" "video" ];
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${userSettings.username} = {
+    uid = 1000;
+    isNormalUser = true;
+    description = userSettings.name;
+    initialPassword = "changeme";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "adbusers"
+      "kvm"
+      "video"
+    ];
+  };
+
+  environment = {
+    # Set environment variables
+    variables = {
+      EDITOR = userSettings.editor;
+      SPAWNEDITOR = userSettings.spawnEditor;
+      TERM = userSettings.terminal;
+      BROWSER = userSettings.browser;
     };
 
-    home-manager.users.${userSettings.username} =  { ... }: {
-        home.stateVersion = "24.05";
+    # Configure XDG user directories
+    etc."xdg/user-dirs.conf".text = ''
+      XDG_MUSIC_DIR="/home/${userSettings.username}/Media/Music"
+      XDG_VIDEOS_DIR="/home/${userSettings.username}/Media/Videos"
+      XDG_PICTURES_DIR="/home/${userSettings.username}/Media/Pictures"
+      XDG_DOWNLOAD_DIR="/home/${userSettings.username}/Downloads"
+      XDG_DOCUMENTS_DIR="/home/${userSettings.username}/Documents"
+      XDG_DESKTOP_DIR="/home/${userSettings.username}/Desktop"
+      XDG_DOTFILES_DIR="/home/${userSettings.username}/.dotfiles"
+      XDG_VM_DIR="/home/${userSettings.username}/VMs"
+    '';
+  };
 
-        home.username = userSettings.username;
-        home.homeDirectory = "/home/"+userSettings.username;
-
-        home.sessionVariables = {
-            EDITOR = userSettings.editor;
-            SPAWNEDITOR = userSettings.spawnEditor;
-            TERM = userSettings.terminal;
-            BROWSER = userSettings.browser;
-        };
-
-        xdg.userDirs = {
-            enable = true;
-            createDirectories = true;
-            music = "/home/${userSettings.username}/Media/Music";
-            videos = "/home/${userSettings.username}/Media/Videos";
-            pictures = "/home/${userSettings.username}/Media/Pictures";
-            download = "/home/${userSettings.username}/Downloads";
-            documents = "/home/${userSettings.username}/Documents";
-            desktop = "/home/${userSettings.username}/Desktop";
-
-            extraConfig = {
-                XDG_DOTFILES_DIR = "/home/${userSettings.username}/.dotfiles";
-                XDG_VM_DIR = "/home/${userSettings.username}/VMs";
-            };
-        };
-
-        # Enable Git
-        programs = {
-            git = {
-                enable = true;
-                lfs.enable = true;
-                userName  = userSettings.name;
-                userEmail = userSettings.email;
-                extraConfig = {
-                    init.defaultBranch = "main";
-                    safe.directory = [
-                        "/etc/nixos"
-                        "/home/${userSettings.username}"
-                    ];
-                };
-            };
-        };
+  # Enable Git
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = userSettings.name;
+      user.email = userSettings.email;
+      init.defaultBranch = "main";
+      safe.directory = [
+        "/etc/nixos"
+        "/home/${userSettings.username}"
+      ];
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
     };
+  };
 }
