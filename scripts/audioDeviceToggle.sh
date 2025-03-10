8f5e4596bd526c15b226b17c5fbca09384099323
Damian Korcz <damiankorcz28@gmail.com>
2025-03-10 02:36:09 +0000

    Updated audioDeviceToggle.sh

--- scripts/audioDeviceToggle.sh
+++ scripts/audioDeviceToggle.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Define your sinks
-DEVICE1="alsa_output.pci-0000_0b_00.4.iec958-stereo"
+DEVICE1="alsa_output.pci-0000_0d_00.4.iec958-stereo"
 DEVICE2="alsa_output.usb-iFi__by_AMR__iFi__by_AMR__HD_USB_Audio_0000-00.iec958-stereo"
+
+# Lock file
+LOCKFILE="/tmp/audioDeviceToggle.lock"
+
+# Create a lock file to prevent multiple instances
+exec 200>$LOCKFILE
+flock -n 200 || exit 1
 
 # Kill previous instances of pactl subscribe, excluding the current one
 pgrep -f "pactl subscribe" | grep -v $$ | xargs -r kill
@@ -29,7 +36,7 @@ for INPUT in $(pactl list sink-inputs short | awk '{print $1}'); do
 done
 
 # Monitor for new sink inputs and move them to the new default sink, except the specified applications
-pactl subscribe | grep --line-buffered "sink-input" | while read -r event; do
+(pactl subscribe | grep --line-buffered "sink-input" | while read -r event; do
     if echo "$event" | grep -q "new"; then
         NEW_INPUT=$(echo "$event" | grep -oP '(?<=#)\d+')
         PROCESS_NAME=$(pactl list sink-inputs | grep -A 20 "Sink Input #$NEW_INPUT" | grep "application.process.binary" | cut -d '"' -f 2)
@@ -37,4 +44,8 @@ pactl subscribe | grep --line-buffered "sink-input" | while read -r event; do
             pactl move-sink-input "$NEW_INPUT" "$NEXT_SINK"
         fi
     fi
-done
+done) &
+
+# Release the lock
+flock -u 200
+rm -f $LOCKFILE
