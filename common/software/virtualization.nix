f15a6ea81e911bfa050c95f3c300810ea2e2a6f9
Damian Korcz <damiankorcz28@gmail.com>
2025-04-26 01:11:26 +0100

    Replaced boxbuddy with DistroShelf

--- common/software/virtualization.nix
+++ common/software/virtualization.nix
@@ -1,59 +1,62 @@
 { pkgs, userSettings, ... }:
 
 {
-    # ------------ Virt Manager ------------
+  # ------------ Virt Manager ------------
 
-    virtualisation.libvirtd = {
+  virtualisation.libvirtd = {
+    enable = true;
+
+    qemu = {
+      package = pkgs.qemu_kvm;
+
+      ovmf = {
         enable = true;
+        packages = [ pkgs.OVMFFull.fd ];
+      };
 
-        qemu = {
-            package = pkgs.qemu_kvm;
-            
-            ovmf = {
-                enable = true;
-                packages = [pkgs.OVMFFull.fd];
-            };
-            
-            swtpm.enable = true;
-        };
+      swtpm.enable = true;
     };
+  };
 
-    programs.virt-manager.enable = true;
+  programs.virt-manager.enable = true;
 
-    users.users.${userSettings.username}.extraGroups = [ "libvirtd" ];
+  users.users.${userSettings.username}.extraGroups = [ "libvirtd" ];
 
-    # ------------ Distrobox ------------
+  # ------------ Distrobox ------------
 
-    virtualisation.podman = {
-        enable = true;
-        dockerCompat = true;
-    };
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
 
-    environment.systemPackages = with pkgs; [
-        distrobox
-        boxbuddy
-     ];
+  environment.systemPackages = with pkgs; [
+    distrobox
+  ];
 
-    # Quadlet?
+  services.flatpak.packages = [
+    "com.ranfdev.DistroShelf"
+  ];
 
-    # For Windows as a Guest:
-    # https://github.com/virtio-win/virtio-win-guest-tools-installer
+  # Quadlet?
 
-    # For NixOS as a Guest:
-    # services.qemuGuest.enable = true;
-    # services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
+  # For Windows as a Guest:
+  # https://github.com/virtio-win/virtio-win-guest-tools-installer
 
-    # Need to enable networking with:
-    # virsh net-start default
-    #
-    # Or autostart:
-    # virsh net-autostart default
+  # For NixOS as a Guest:
+  # services.qemuGuest.enable = true;
+  # services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
 
-    # https://nixos.wiki/wiki/Virt-manager
-    #     dconf.settings = {
-    #         "org/virt-manager/virt-manager/connections" = {
-    #             autoconnect = ["qemu:///system"];
-    #             uris = ["qemu:///system"];
-    #         };
-    #     };
+  # Need to enable networking with:
+  # virsh net-start default
+  #
+  # Or autostart:
+  # virsh net-autostart default
+
+  # https://nixos.wiki/wiki/Virt-manager
+  #     dconf.settings = {
+  #         "org/virt-manager/virt-manager/connections" = {
+  #             autoconnect = ["qemu:///system"];
+  #             uris = ["qemu:///system"];
+  #         };
+  #     };
 }
